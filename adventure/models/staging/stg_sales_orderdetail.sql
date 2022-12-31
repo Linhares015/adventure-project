89c3ac4f8c37d83d59bd34a7685e600bdc7985b6
linhares <tiagolinhares051@gmail.com>
2022-12-31 14:21:58 -0300

    primeira parte das stg

---
+++ adventure/models/staging/stg_sales_orderdetail.sql
@@ -0,0 +1,19 @@
+with 
+    source_sales_orderdetail as (
+        select
+            orderqty
+            ,salesorderid
+            ,salesorderdetailid
+            ,productid
+            ,specialofferid
+            ,unitprice
+            ,unitpricediscount
+            ,linetotal
+            ,rowguid
+            ,modifieddate
+            ,carriertrackingnumber
+        from {{ source('adventure', 'sales_orderdetail') }}
+    )
+
+select *
+from source_sales_orderdetail
