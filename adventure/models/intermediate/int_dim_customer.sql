4a185acc112b142e82e596de0796407eeebe2fcd
linhares <tiagolinhares051@gmail.com>
2023-01-21 12:02:39 -0300

    ajustando umas stg e a customer do intermidiate

--- adventure/models/intermediate/int_dim_customer.sql
+++ adventure/models/intermediate/int_dim_customer.sql
@@ -4,11 +4,39 @@ with
         from {{ source('stg', 'stg_sales_customer') }}
     )
 
-    ,transformacoes as (
+    ,store as (
+        select *
+        from {{ source('stg', 'stg_sales_store') }}
+    )
+
+    ,salesperson as (
+        select *
+        from {{ source('stg', 'stg_sales_salesperson') }}
+    )
+
+    ,salesterritory as (
+        select *
+        from {{ source('stg', 'stg_sales_territory') }}
+    )
+
+    ,joined as (
         select 
-            *
+            customer.customerid
+            --,customer.personid
+            --,customer.territoryid
+            --,store.businessentityid
+            ,store.salespersonid
+            --,store.demographics
+            ,store.name_store
+            --,salesperson.territoryid 
+            ,salesterritory.name_territory
+            ,salesterritory.countryregioncode
+            ,salesterritory.group_territory
         from customer
+        left join store on customer.storeid = store.businessentityid
+        left join salesperson on store.salespersonid = salesperson.businessentityid
+        left join salesterritory on salesperson.territoryid = salesterritory.territoryid
     )
 
 select *
-from transformacoes
+from joined
