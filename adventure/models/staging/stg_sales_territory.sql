10eb772be39c6f0e4c411d3b7a974351f3f0e85e
linhares <tiagolinhares051@gmail.com>
2023-02-01 15:37:25 -0300

    Ajustando as dimensões

--- adventure/models/staging/stg_sales_territory.sql
+++ adventure/models/staging/stg_sales_territory.sql
@@ -8,9 +8,9 @@ with
             --,costlastyear
             --,rowguid
             --,modifieddate
-            ,"Name" as name_territory
+            ,name as name_territory
             ,countryregioncode
-            ,"Group" as group_territory
+            ,group_territory
         from {{ source('adventure', 'raw_sales_territory') }}
     )
 
